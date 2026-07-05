01744ec89925eaf30b2b3e5b2efb7888713dc937
pvlznl <pvl.znl@mail.ru>
2026-07-05 09:37:59 +0500

    add models for category and products

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine  # type: ignore
-from sqlalchemy.ext.declarative import declarative_base  # type: ignore
-from sqlalchemy.orm import sessionmaker  # type: ignore
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
